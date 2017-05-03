f874db22991065cc412da3a09b0b8e3e24abed1d
osotorrio <osotorrio@hotmail.com>
2017-05-03 19:46:54 +0100

    Exercise 6

--- scripts/exercises/exercise-5.js
+++ scripts/exercises/exercise-5.js
@@ -6,9 +6,9 @@
 --------------------
 
 	Write a function called "hasTeen" which takes the following parameters:
-		- x: number
-		- y: number
-		- z: number
+		- x: Number
+		- y: Number
+		- z: Number
 		
 	We'll say that a number is "teen" if it is in the range 13..19 inclusive. 
 	Given 3 int values, return true if 1 or more of them are teen.
