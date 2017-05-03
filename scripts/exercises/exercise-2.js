138f68f5630fb4fdd563f69182cc6f01155a0b1a
gsotorrio <willybosko@gmail.com>
2017-05-03 18:16:59 +0200

    Exercise 2 done

--- scripts/exercises/exercise-2.js
+++ scripts/exercises/exercise-2.js
@@ -12,3 +12,13 @@
 	the function returns the absolute difference between X and 21, 
 	except returns double the absolute difference if X is over 21.
 */
+
+var diff21 = (number) => {
+	let result = 21 - number;
+	
+	if (number > 21){
+		result = (number - 21)*2;
+	}
+	
+	return result;
+};
