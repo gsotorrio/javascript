1151f6305c3effafccc62fd59a8d768ce1867589
gsotorrio <willybosko@gmail.com>
2017-05-12 08:38:39 +0200

    Exercise 7 ...

--- scripts/exercises/exercise-7.js
+++ scripts/exercises/exercise-7.js
@@ -11,3 +11,12 @@
 	
 	Return true if the given string contains between 1 and 3 'e' chars
 */
+
+const stringWithE = (text) => {
+	
+	if (!text || (text.match(/e/g)).length > 3 || (text.split("e").length -1) == 0){
+		return false;
+	}
+	
+	return (text.slice(0,1) == "e" || text.slice(1,2) == "e" || text.slice(2,3) == "e");	
+}
