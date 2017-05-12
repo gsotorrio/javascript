3adfd93a7ba7b8cd5d2bcf31a38d28cd1b6e3f22
osotorrio <osotorrio@hotmail.com>
2017-05-12 19:16:12 +0100

    Exercise 14

---
+++ scripts/exercises/exercise-14.js
@@ -0,0 +1,16 @@
+"use strict"
+
+/* 
+--------------------
+	EXERCISE 14
+--------------------
+	
+	Write a function called "loneTeen" which takes the following parameters:
+		- x: Number
+		- y: Number
+
+	We'll say that a number is "teen" if it is in the range 13..19 inclusive. 
+	Given 2 int values, return true if one or the other is teen, but not both.
+*/
+
+
