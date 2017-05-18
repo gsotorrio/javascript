84869ed31b486a7ed82a100053d27a5d458814b8
gsotorrio <willybosko@gmail.com>
2017-05-18 09:18:16 +0200

    Exercise 7 done

--- scripts/exercises/exercise-7.js
+++ scripts/exercises/exercise-7.js
@@ -14,7 +14,7 @@
 
 const stringWithE = (text) => {
 	
-	if (!text || (text.match(/e/g)).length > 3 || text.replace(/[^e]/g, "").length == 0){
+	if (!text || text.replace(/[^e]/g, "").length > 3 || text.replace(/[^e]/g, "").length == 0){
 		return false;
 	}
 	
