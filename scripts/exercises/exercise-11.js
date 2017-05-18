c61f6c39e1d3078b6894dda4cc7e9e2f91e15dcd
gsotorrio <willybosko@gmail.com>
2017-05-18 10:21:33 +0200

    Exercise 11 done

--- scripts/exercises/exercise-11.js
+++ scripts/exercises/exercise-11.js
@@ -12,5 +12,15 @@
 */
 
 const frontBack = (text) => {
+	if (!text){
+		return "";
+	}
+	
 	var array = text.split("");
+	var firstCharacter = array.shift();
+	var lastCharacter = array.splice(-1,1);
+	array.unshift(lastCharacter[0]);
+	array.push(firstCharacter);
+	
+	return array.join("");
 }
