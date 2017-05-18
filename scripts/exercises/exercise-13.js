dd1836d086ec0a17d9aec8d8bb228e411ef5a8ac
gsotorrio <willybosko@gmail.com>
2017-05-18 11:00:36 +0200

    Exercise 13 done

--- scripts/exercises/exercise-13.js
+++ scripts/exercises/exercise-13.js
@@ -12,3 +12,11 @@
 	
 	Given two temperatures, return true if one is less than 0 and the other is greater than 100.
 */
+
+const icyHot = (temp1, temp2) => {
+	if(temp1 > 100 && temp2 > 100 || temp1 == 100 && temp2 > 100 || temp1 < 0 && temp2 == 0 || temp1 < 0 && temp2 < 0){
+		return false;
+	}
+	
+	return (temp1 < 0 || temp2 < 0 && temp1 > 100 || temp2 > 100);
+}
