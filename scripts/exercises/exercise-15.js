22ff53502e2d9cca7e699c9a9a3e6529e4cc7277
gsotorrio <willybosko@gmail.com>
2017-05-20 08:14:06 +0200

    Exercise 15 done

--- scripts/exercises/exercise-15.js
+++ scripts/exercises/exercise-15.js
@@ -12,3 +12,18 @@
 	However, include first char only if it is 'o'.
 	And include the second only if it is 'z'.	
 */
+
+const startOz = (text) => {
+	
+	if(text.slice(0,2) == "oz"){
+		return "oz";
+	}
+	if(text.slice(0,1) == "o" && text.slice(1,2) != "z"){
+		return "o";
+	}
+	if(text.slice(0,1) != "o" && text.slice(1,2) == "z"){
+		return "z";
+	}
+	
+	return "";
+}
