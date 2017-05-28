7bb04baa6b0562e68c00386fd3fac1dea9f2f4d7
gsotorrio <willybosko@gmail.com>
2017-05-28 10:25:31 +0200

    Exercise 18 done

--- scripts/exercises/exercise-18.js
+++ scripts/exercises/exercise-18.js
@@ -14,3 +14,11 @@ Given two int values, return their sum. Unless the two values are the same,
 then return double their sum.
 */
 
+const sumDouble = (x, y) => {
+	if (x != y) {
+		return x + y;
+	}
+	
+	return (x + y)*2;
+}
+
